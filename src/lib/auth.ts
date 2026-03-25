a07d0161a4d4fd31265bb239929c1d9ba533fbc4
sobhan <sobhans@mindfiresolutions.com>
2026-03-25 17:17:45 +0530

    feat(api): impliment better auth email provider and intigrate with the frontend

---
+++ src/lib/auth.ts
@@ -0,0 +1,35 @@
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from '@/database/index';
+import { accounts } from '@/database/schema/accounts.schema';
+import { users } from '@/database/schema/user.schema';
+import { sessions } from '@/database/schema/session.schema';
+import { verifications } from '@/database/schema/verification.schema';
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    schema: {
+      user: users,
+      session: sessions,
+      account: accounts,
+      verification: verifications,
+    },
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: false,
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+    updateAge: 60 * 60 * 24,
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60,
+    },
+  },
+  trustedOrigins: [process.env.NEXT_PUBLIC_APP_URL ?? 'http://localhost:3000'],
+});
+
+export type Session = typeof auth.$Infer.Session;
+export type User = typeof auth.$Infer.Session.user;
