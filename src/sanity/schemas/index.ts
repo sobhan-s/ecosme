16bf6a4d45c5e2b910ba6ef4f63e6475d839b2ce
sobhan <sobhans@mindfiresolutions.com>
2026-03-27 08:37:28 +0530

    feat(schema): add root index file for all schema for singleton

---
+++ src/sanity/schemas/index.ts
@@ -0,0 +1,19 @@
+import { siteSettings } from './siteSetting';
+import { heroSection } from './heroSeciton';
+import { aboutSection } from './aboutSecion';
+import { workItem } from './workItem';
+import { pricingPlan } from './pricing';
+import { testimonial } from './testimonial';
+import { customerLogo } from './customer';
+import { post } from './post';
+
+export const schemaTypes = [
+  siteSettings,
+  heroSection,
+  aboutSection,
+  workItem,
+  pricingPlan,
+  testimonial,
+  customerLogo,
+  post,
+];
